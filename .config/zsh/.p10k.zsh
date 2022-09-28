f0acf70b10dcb9483fe40b500b338663bfd30168
Xavier Mitault <xavier.mitault@epitech.eu>
2022-09-28 20:29:18 +0200

    Add option to configure config for git in bootstrap; Split on newline kubecontext and language versions

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -48,6 +48,9 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     background_jobs         # presence of background jobs
+    kubecontext             # current kubernetes context (https://kubernetes.io/)
+    # =========================[ Line #2 ]=========================
+    newline
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
@@ -75,7 +78,6 @@
     # phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
     # scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
     # haskell_stack           # haskell version from stack (https://haskellstack.org/)
-    kubecontext             # current kubernetes context (https://kubernetes.io/)
     # terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version       # terraform version (https://www.terraform.io)
     # aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
@@ -101,7 +103,7 @@
     # todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     # timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    # =========================[ Line #2 ]=========================
+    # =========================[ Line #3 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
