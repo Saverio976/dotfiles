c00c52678ff7eecbcbdf67e0d6c4e6fbac378891
Xavier Mitault <xavier.mitault@epitech.eu>
2022-09-29 19:44:33 +0200

    Add bash_color to downloadutils

--- .local/bin/download-utils.sh
+++ .local/bin/download-utils.sh
@@ -1,8 +1,8 @@
 echo "Do you want omyzsh"
 echo "bash script: https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
-echo "[Y/n] "
+echo "[y/N] "
 read CHOICE
-if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]; then
+if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]]; then
     if [[ "$ZSH" == "" ]]; then
     export ZSH="$XDG_CONFIG_HOME/oh-my-zsh"
     fi
@@ -11,18 +11,18 @@ fi
 
 echo "Do you want a better docker purge (binary name -> dkpurge)"
 echo "bash script: https://raw.githubusercontent.com/AustralEpitech/.dotfiles/work/bin/dkpurge"
-echo "[Y/n] "
+echo "[y/N] "
 read CHOICE
-if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]; then
+if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]]; then
     curl https://raw.githubusercontent.com/AustralEpitech/.dotfiles/work/bin/dkpurge > $XDG_LOCAL_BIN_HOME/dkpurge
     chmod +x $XDG_LOCAL_BIN_HOME/dkpurge
 fi
 
 echo "Do you want neovim appimage ("'$VERSION' by default is v0.7.2 but you cant set it")"
 echo "downloader link: https://github.com/neovim/neovim/releases/download/"'$VERSION'"/nvim.appimage"
-echo "[Y/n]"
+echo "[y/N]"
 read CHOICE
-if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]; then
+if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]]; then
     echo "Which version (tag release): [v0.7.2] "
     read NVIM_VERSION
     if [[ "$NVIM_VERSION" == "" ]]; then
@@ -34,8 +34,17 @@ fi
 
 echo "Do you want zoxide (smart cd)"
 echo "downloader link: https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh"
-echo "[Y/n]"
+echo "[y/N]"
 read CHOICE
-if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]; then
+if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]]; then
     curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+fi
+
+echo "Do you want bash_color (show color number in terminal)"
+echo "file: https://raw.githubusercontent.com/Remi-Mergen/bash_color/master/color.sh"
+echo "[y/N]"
+read CHOICE
+if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]]; then
+    curl -Lo "$XDG_LOCAL_BIN_HOME/color.sh" https://raw.githubusercontent.com/Remi-Mergen/bash_color/master/color.sh
+    chmod +x "$XDG_LOCAL_BIN_HOME/color.sh"
 fi
