0e784c5c96378418b91df9f4fe8183167decb7f0
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-02 15:40:57 +0200

    Fix setup of indentomatic

--- .config/nvim/lua/user/text-interract/text-interract.lua
+++ .config/nvim/lua/user/text-interract/text-interract.lua
@@ -8,5 +8,9 @@ end
 
 local okindentomatic, indentomatic = pcall(require, 'indent-o-matic')
 if okindentomatic then
-    indentomatic.setup()
+    indentomatic.setup({
+        max_lines = 2048,
+        standard_widths = { 2, 4, 8 },
+        skip_multiline = true,
+    })
 end
