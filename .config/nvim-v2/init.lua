30f2172f25950f4e41492cc291f05b63451b64d3
Xavier Mitault <xavier.mitault@epitech.eu>
2023-02-07 16:39:28 +0100

    Remove check neovim version

--- .config/nvim-v2/init.lua
+++ .config/nvim-v2/init.lua
@@ -1,22 +1,5 @@
 local api = vim.api
 
--- https://github.com/jdhao/nvim-config/blob/master/lua/utils.lua
-function get_nvim_version()
-    local actual_ver = vim.version()
-
-    local nvim_ver_str = string.format("%d.%d.%d", actual_ver.major, actual_ver.minor, actual_ver.patch)
-    return nvim_ver_str
-end
-local expected_ver = "0.8.2"
-local nvim_ver = get_nvim_version()
-
--- https://github.com/jdhao/nvim-config/blob/master/init.lua
-if nvim_ver ~= expected_ver then
-    local msg = string.format("Unsupported nvim version: expect %s, but got %s instead!", expected_ver, nvim_ver)
-    api.nvim_err_writeln(msg)
-    return
-end
-
 -- impatient
 _G.__luacache_config = {
     chunks = {
