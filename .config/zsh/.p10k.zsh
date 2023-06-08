5498a99db29e47951259293571e3bb10e270970e
Xavier Mitault <xavier.mitault@epitech.eu>
2023-06-08 17:19:25 +0200

    Conf: somz regular funcs

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -116,7 +116,7 @@
   # Show previous command duration only if it's >= 5s.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=0
   # Don't show fractional seconds. Thus, 7s rather than 7.3s.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=3
   # Duration format: 1d 2h 3m 4s.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FORMAT='d h m s'
   # Yellow previous command duration.
