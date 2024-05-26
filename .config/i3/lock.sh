cb948f189975a6855dee96574b06c881ba99f912
Xavier Mitault <xavier.mitault@epitech.eu>
2024-05-26 17:27:16 +0200

    I3: fix locker for suspend

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -2,19 +2,31 @@
 
 bg_color="$1"
 bg_image="$2"
-i3lock_option="$3"
+nofork="$3"
 
 revert() {
     xset -dpms
 }
-trap revert HUP INT TERM
-xset +dpms dpms 5 5 5
-i3lock \
-    --color "$bg_color" \
-    --image "$bg_image" \
-    --show-failed-attempts \
-    --ignore-empty-password \
-    --show-keyboard-layout \
-    --tiling \
-    $i3lock_option
-revert
+
+if [ "$nofork" = "--nofork" ]; then
+    trap revert HUP INT TERM
+    xset +dpms dpms 5 5 5
+    i3lock \
+        --color "$bg_color" \
+        --image "$bg_image" \
+        --show-failed-attempts \
+        --ignore-empty-password \
+        --show-keyboard-layout \
+        --tiling \
+        $nofork
+    revert
+else
+    i3lock \
+        --color "$bg_color" \
+        --image "$bg_image" \
+        --show-failed-attempts \
+        --ignore-empty-password \
+        --show-keyboard-layout \
+        --tiling \
+        && (systemctl suspend-then-hibernate || systemctl suspend)
+fi
